de07f6f6b8a3efc8968feef34077baa0a15fba23
HARISGANZ <harisnew675@gmail.com>
2021-12-28 02:37:11 +0700

    testok

---
+++ media/help.js
@@ -0,0 +1,149 @@
+const help = (prefix) => {
+	return` Hai👋 Im ${botname}:)
+┌─❑「 INFO DEVELOPER 」
+│👤 Nama : Haris
+│📃 Github : Cari Sendiri!?
+│▶ Yutub : HANZ々OFC彡
+│💮 Ig : @wong_koclok 
+└─❑
+
+┌─❑
+│「 FULL MENU KEBAWAH 」
+└┬❑
+    │ 「 Self And Public」
+┌┤冬  >public
+││冬  >self 
+│└────────────┈ ⳹
+│ 「 GROUP MENU」
+│冬 ${prefix}welcome on/off
+│冬 ${prefix}antilink
+│冬 ${prefix}antivirtex
+│冬 ${prefix}add
+│冬 ${prefix}kick
+│冬 ${prefix}promote
+│冬 ${prefix}demote
+│冬 ${prefix}tagall
+│冬 ${prefix}hidetag
+│冬 ${prefix}setpp
+│冬 ${prefix}setdes
+│冬 ${prefix}setname
+│─────────────┈ ⳹
+│ 「 OWNER MENU」
+│冬 ${prefix}set
+│冬 ${prefix}auto
+│冬 ${prefix}bc
+│冬 ${prefix}bc2
+│冬 ${prefix}tobc
+│冬 ${prefix}jadibot
+│冬 ${prefix}stopjadibot
+│冬 ${prefix}addcmd
+│冬 ${prefix}delcmd
+│冬 ${prefix}listcmd
+│冬 ${prefix}restart
+│冬 ${prefix}autorespon pc/gc
+│冬 ${prefix}setppbot
+│冬 ${prefix}setppwa
+│冬 ${prefix}upswtext
+│冬 ${prefix}upswvn
+│冬 ${prefix}upswvideo
+│冬 ${prefix}upswimage
+│冬 ${prefix}upswgif
+│冬 ${prefix}upswaudio
+│冬 ${prefix}upswsticker
+│冬 ${prefix}upswlokasi
+│─────────────┈ ⳹
+│ 「 MAKER MENU 」
+│冬 ${prefix}neon
+│冬 ${prefix}quote
+│冬 ${prefix}glow
+│冬 ${prefix}burn
+│冬 ${prefix}flower
+│冬 ${prefix}wooden
+│冬 ${prefix}golden
+│冬 ${prefix}summer
+│冬 ${prefix}maker2d2
+│冬 ${prefix}maker2d3
+│冬 ${prefix}maker2d4
+│冬 ${prefix}maker3d2
+│冬 ${prefix}maker3d3
+│冬 ${prefix}maker3d4
+│冬 ${prefix}transformer
+│冬 ${prefix}googletxt
+│冬 ${prefix}coffeecup
+│冬 ${prefix}battlefield
+│─────────────┈ ⳹
+│ 「 IMAGE MENU」
+│冬 ${prefix}attp
+│冬 ${prefix}toimg
+│冬 ${prefix}tovideo
+│冬 ${prefix}tomp3
+│冬 ${prefix}sticker
+│─────────────┈ ⳹
+│ 「 MUSIC MENU」
+│冬 ${prefix}sound1
+│冬 ${prefix}sound2
+│冬 ${prefix}sound3
+│冬 ${prefix}sound4
+│冬 ${prefix}sound5
+│冬 ${prefix}sound6
+│冬 ${prefix}sound7
+│冬 ${prefix}sound8
+│冬 ${prefix}sound9
+│冬 ${prefix}sound10
+│─────────────┈ ⳹
+│ 「 RANDOM MENU」
+│冬 ${prefix}detikvn
+│冬 ${prefix}detikvideo
+│冬 ${prefix}ttmusik
+│冬 ${prefix}play
+│冬 ${prefix}darkjokes
+│─────────────┈ ⳹
+│ 「 18+ MENU」
+│冬 ${prefix}kodenuklir
+│冬 ${prefix}linkbokep
+│冬 ${prefix}bokep1
+│冬 ${prefix}bokep2
+│冬 ${prefix}bokep3
+│冬 ${prefix}bokep4
+│冬 ${prefix}bokep5
+│冬 ${prefix}bokep6
+│冬 ${prefix}bokep7
+│冬 ${prefix}bokep8
+│冬 ${prefix}bokep9
+│冬 ${prefix}bokep10
+│冬 ${prefix}bokep11
+│冬 ${prefix}bokep12
+│冬 ${prefix}bokep13
+│冬 ${prefix}bokep14
+│冬 ${prefix}bokep15
+│─────────────┈ ⳹
+│ 「 WAR MENU」
+│冬 ${prefix}troli
+│冬 ${prefix}troliv2
+│冬 ${prefix}virtex
+│冬 ${prefix}bugpdf
+│冬 ${prefix}bugdoc
+│冬 ${prefix}bugloc
+│冬 ${prefix}hanzbug
+│冬 ${prefix}hanzofc
+│冬 ${prefix}bugbutton
+│冬 ${prefix}jadivirus
+│冬 ${prefix}jadivirvid
+│冬 ${prefix}jadikatalog
+│冬 ${prefix}tovirgam
+└┬────────────┈ ⳹
+┌┤➤ DEVELOPER 
+││
+││Menu Dikit✌
+│├──────── 
+││冬 ◗ _Hanz Ofc_
+│└──────── 
+│冬◗ My Team WH Team
+└─━━━─── • ───━━━❋ཻུ۪۪⸙
+
+║▌│█║▌│ █║▌│█│║▌║
+║▌│█║▌│ █║▌│█│║▌║
+      「 Bot Wea」
+`
+}
+exports.help = help
